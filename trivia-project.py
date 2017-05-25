1a12a063663206cb675c1eaa4d2abae031fa8788
Priyanka Advani <padvani89@gmail.com>
2017-05-25 20:50:48 +0000

    Added more comments & docstrings

--- trivia-project.py
+++ trivia-project.py
@@ -1,28 +1,28 @@
 CATEGORY_1_Q_AND_A = {
-    "Crebase A) Pokemon B) Big Data C) Prescription Drug": "A",
-    "Tokutek A) Pokemon B) Big Data C) Prescription Drug": "B",
-    "Adabas A) Pokemon B) Big Data C) Prescription Drug": "B",
-    "Feebas A) Pokemon B) Big Data C) Prescription Drug": "A",
-    "Actos A) Pokemon B) Big Data C) Prescription Drug": "C",
-    "Decadron A) Pokemon B) Big Data C) Prescription Drug": "C" }
+    "Crebase\nA) Pokemon B) Big Data C) Prescription Drug": "A",
+    "Tokutek\nA) Pokemon B) Big Data C) Prescription Drug": "B",
+    "Adabas\nA) Pokemon B) Big Data C) Prescription Drug": "B",
+    "Feebas\nA) Pokemon B) Big Data C) Prescription Drug": "A",
+    "Actos\nA) Pokemon B) Big Data C) Prescription Drug": "C",
+    "Decadron\nA) Pokemon B) Big Data C) Prescription Drug": "C" }
 
 
 CATEGORY_2_Q_AND_A = {
-    "2000 A) Crouching Tiger, Hidden Dragon B) Gladiator C) Traffic": "B",
-    "2001 A) A Beautiful Mind B) Moulin Rouge! C) LOTR: The Fellowship of the Ring": "A",
-    "2002 A) Chicago B) Gangs of New York C) The Pianist": "A",
-    "2003 A) Lost in Translation B) LOTR: The Return of the King C) Seabiscuit": "B",
-    "2004 A) The Aviator B) Finding Neverland C) Million Dollar Baby": "C",
-    "2005 A) Capote B) Brokeback Mountain C) Crash": "C" }
+    "2000\nA) Crouching Tiger, Hidden Dragon B) Gladiator C) Traffic": "B",
+    "2001\nA) A Beautiful Mind B) Moulin Rouge! C) LOTR: The Fellowship of the Ring": "A",
+    "2002\nA) Chicago B) Gangs of New York C) The Pianist": "A",
+    "2003\nA) Lost in Translation B) LOTR: The Return of the King C) Seabiscuit": "B",
+    "2004\nA) The Aviator B) Finding Neverland C) Million Dollar Baby": "C",
+    "2005\nA) Capote B) Brokeback Mountain C) Crash": "C" }
 
 
 CATEGORY_3_Q_AND_A = {
-    "The fear of snakes A) Ophidiophobia B) Entomophobia C) Selachophobia": "B",
-    "The fear of crowds A) Chiroptophobia B) Enochlophobia C) Oneirophobia": "B",
-    "The fear of being robbed A) Aurophobia B) Harpaxophobia C) Ichthyophobia": "B",
-    "The fear of isolation A) Zelophobia B) Urophobia C) Autophobia": "C",
-    "The fear of freedom A) Myrmecophobia B) Dendrophobia C) Eleutherophobia": "C",
-    "The fear of mice A) Herpetophobia B) Musophobia C) Neophobia": "B" }
+    "Fear of snakes\nA) Ophidiophobia B) Entomophobia C) Selachophobia": "B",
+    "Fear of crowds\nA) Chiroptophobia B) Enochlophobia C) Oneirophobia": "B",
+    "Fear of being robbed\nA) Aurophobia B) Harpaxophobia C) Ichthyophobia": "B",
+    "Fear of isolation\nA) Zelophobia B) Urophobia C) Autophobia": "C",
+    "Fear of freedom\nA) Myrmecophobia B) Dendrophobia C) Eleutherophobia": "C",
+    "Fear of mice\nA) Herpetophobia B) Musophobia C) Neophobia": "B" }
 
 
 # for printing in color
@@ -40,61 +40,83 @@ def calculate_score(original_score, number_of_guesses):
     # negative scores are not possible
     if number_of_guesses <= 4:
         current_question_score = (4 - number_of_guesses)
+    
     else:
         current_question_score = 0
+    
     new_score = original_score + current_question_score
+    
     return new_score
 
 
 def choose_category():
-    """ Asks user to choose a category """
+    """ Asks user to choose a category, validates input,
+    and returns the chosen category """
 
     while True:
+
         print color_gray.format("\nCategories: \n1) Pokemon, Big Data, or Prescription Drug? \n2) Oscar Winning Movies (2000s) \n3) Phobias")
         user_category_choice = raw_input(color_blue.format("\nPlease choose a category: "))
+        
         if user_category_choice.isdigit():
+            
             if int(user_category_choice) == 1:
                 print highlight_gray.format("\nCategory 1: Is it a Pokemon, big data technology, or prescription drug?")
                 chosen_category = CATEGORY_1_Q_AND_A
                 return chosen_category
+            
             elif int(user_category_choice) == 2:
                 print highlight_gray.format("\nCategory 2: Which movie won the Oscar for Best Picture in each year?")
                 chosen_category = CATEGORY_2_Q_AND_A
                 return chosen_category
+            
             elif int(user_category_choice) == 3:
                 print highlight_gray.format("\nCategory 3: Can you identify the phobia by its definition?")
                 chosen_category = CATEGORY_3_Q_AND_A
                 return chosen_category
+            
             else:
-                print color_red.format("\n Sorry, that is not a valid category")
+                print color_red.format("\nSorry, that is not a valid category")
+        
         else:
-            print color_red.format("\n Sorry, that is not a valid category")
+            print color_red.format("\nSorry, that is not a valid category")
 
 
 def play_trivia():
     """ Takes user category choice and loops through questions for that category """
     
+    # user score starts at 0 at the beginning of each instance of game play
     user_score = 0
+
+    # use choose_category function to assign the user's choice to chosen_category
     chosen_category = choose_category()
+
+    # iterate through dictionary of Q&As for the chosen question
     for question in chosen_category:
+        
         user_answer = (raw_input(color_gray.format("\n" + question) + color_blue.format("\nYour answer: "))).upper()
         user_guesses = 1
+        
         while user_answer != chosen_category[question]:
             print color_red.format("\nSorry, try again")
             user_answer = (raw_input(color_gray.format("\n" + question) + color_blue.format("\nYour answer: "))).upper() # make this a function
             user_guesses = user_guesses + 1
             continue
+        
         print color_green.format("\nThat's right!")
         user_score = calculate_score(user_score, user_guesses)
         print color_gray.format("Your score is " + str(user_score))
+    
     print highlight_gray.format("\nThanks for playing! Your final score is " + str(user_score))
 
 
 def start_game():
-    """ Starts the game by asking the user if they want to play """
+    """ Asks the user if they want to play, validates input,
+    and either starts game or exits """
 
     while True:
         user_choice = raw_input(color_blue.format("\nDo you want to play trivia? Y/N: "))
+        
         if user_choice.upper() == "Y":
             print color_gray.format("\nGreat, let's get started!")
             print color_gray.format("\nRules:")
@@ -103,10 +125,16 @@ def start_game():
             print color_gray.format("- If it takes you four or more guesses to get the answer right, you will get 0 points")
             print color_gray.format("- Invalid answer choices count towards your number of guesses")
             print color_gray.format("- You must answer each question in order to get to the next question")
+            
+            # starts game play
             play_trivia()
+        
         elif user_choice.upper() == "N":
-            print color_gray.format("\nMaybe next time. Goodbye!")
+            print color_gray.format("\nMaybe next time. Goodbye!\n")
             break
+        
+        else:
+            print color_red.format("\n Sorry, that is not a valid choice")
 
 
 start_game()
